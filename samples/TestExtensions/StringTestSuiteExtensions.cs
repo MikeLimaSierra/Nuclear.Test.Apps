0ba27595d86b989e1807a368c5286d0fbc5b0029
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-01-08 08:56:22 +0100

    extend stringtestsuite

---
+++ samples/TestExtensions/StringTestSuiteExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+using Nuclear.Extensions;
+using Nuclear.TestSite.TestSuites;
+
+namespace TestExtensions {
+    public static class StringTestSuiteExtensions {
+
+        public static void IsHelloWorld(this StringTestSuite _this, String @string,
+            String customMessage = null, [CallerFilePath] String _file = null, [CallerMemberName] String _method = null) {
+
+            if(@string == null) {
+                _this.FailTest($"Parameter '{nameof(@string)}' is null.", _file, _method);
+                return;
+            }
+
+            _this.InternalTest(@string == "Hello World!", $"[String = {@string.Format()}]", customMessage, _file, _method);
+        }
+
+    }
+}
